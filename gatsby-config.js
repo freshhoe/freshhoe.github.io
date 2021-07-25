fe820a02991855638eea1f1b9f8a30f250e584bd
freshhoe <yh91211@gmail.com>
2021-07-25 22:16:54 +0900

    fix: Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,18 +84,12 @@ module.exports = {
         policy: [{ userAgent: '*', allow: '/' }],
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: `src/favicon-32x32.png`, // This path is relative to the root of the site.
+      },
+    },
     // `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
