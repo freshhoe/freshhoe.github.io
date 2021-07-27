1610f8061323a92747729d108385b9a513dee9c2
freshhoe <yh91211@gmail.com>
2021-07-27 20:35:10 +0900

    Algorithm 0726 quiz

--- contents/Algorithm/0726/0726_quiz.js
+++ contents/Algorithm/0726/0726_quiz.js
@@ -1,36 +1,29 @@
-// function solution(words) {
-
-//     return answer;
-// }
-// console.log(solution(["steasue", "sasseysu", "kseseas"]));
-// console.log(solution(["ackky", "kabck", "yokkcs"]));
-
-let words = ["steasue", "sasseysu", "kseseas"];
-words.sort((a,b) => a.length - b.length);
-let answer = [];
-let len = words.length;
-
-// console.log(words[0].split('')[0]);
-
-function minEqual(short, long) {
-    let sh = new Map(short);
-    for (i = 0; i < short.length; i++) {
-        for (j = 0; j < long.length; j++) {
-            if (short[i] === long[j]) {
-
+function solution(words) {
+    words.sort((a,b) => a.length - b.length);
+    let answer = [];
+    let len = words.length;
+    let sH = new Map ();
+    for (let x of words[0]) {
+        sH.set(x, sH.get(x) + 1 || 1);
+    }
+    for (let i = 1; i < words.length; i++) {
+        let tmp = new Map ();
+        for (let x of words[i]) {
+            if(sH.get(x)) {
+                tmp.set(x, tmp.get(x) + 1 || 1);
+                sH.set(x, sH.get(x) - 1);
             }
         }
+        sH = tmp;
     }
-    for (let x of short) {
-        if (long.indexOf(x) !== -1) return false;
-    return true;
-}
 
-for (let i = 0; i < len - 1; i++) {
-    for (let j = i + 1; j < len; j++) {
-        if (isUnique(words[i], words[j])) {
-             let tmp = words[i].length * words[j].length;
-             answer.push();
-        }   
+    let str ="";
+    for (let [key, val] of sH) {
+        str += key.repeat(val);
     }
+    answer = str.split('');
+
+    return answer;
 }
+console.log(solution(["steasue", "sasseysu", "kseseas"]));
+console.log(solution(["ackky", "kabck", "yokkcs"]));
