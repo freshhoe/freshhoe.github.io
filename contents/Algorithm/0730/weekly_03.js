b71f86229c0eb74ac1f66fe92c2d43cb8c99a12f
freshhoe <yh91211@gmail.com>
2021-07-31 12:50:18 +0900

    Fix: modefied Weekly test 03

--- contents/Algorithm/0730/weekly_03.js
+++ contents/Algorithm/0730/weekly_03.js
@@ -1,93 +1,28 @@
 function solution (s) {
     let answer = '';
-    let num = '';
-    let str = '';
+    s = s.split('');
+    let stack = [];
+    let num = [];
     
-    for (let x of s) {
-        if (Number(x)) num += x;
-        if (isNaN(Number(x)) && x !== '(' && x !== ')')  {
-            str += x;
-            if (num.length === 0) {
-                answer += str;
-                str='';
+    for (let i = 0; i < s.length; i++) {
+        if (Number(s[i])) { // 별도의 스택에 따로 반복횟수 push
+            if (Number(s[i+1])) num.push(Number(s[i] + s[i+1])); // 두 자리 숫자면 합쳐서 push
+            num.push(s[i]);
+            continue;
+        } else if (s[i] !== ')') {
+            stack.push(s[i]);
+        } else if (s[i] === ')') {
+            let str = []; // 별도의 스택에 문자열 push
+            str.push(stack.splice(stack.lastIndexOf('(') + 1).join('')); // '(' 앞 문자부터 스택에 쌓인 문자열 끝까지 배열이 아닌 문자열로 만들어 push
+            stack.pop(); // '(' 제거
+            for (let i = 0; i < num[num.length-1]; i++) {
+                stack.push(str); // num stack의 마지막 숫자만큼 str 에 있는 문자열을 반복해서 stack에 push
             }
-        }    
-        if (x === ')') {
-            for (let i = 0; i < num; i++) {
-                answer += str;
-            }
-            num = '';
-            str = '';
+            num.pop(); // 사용한 반복횟수는 제거
         }
     }
-    return answer;
+    return answer = stack.join('');
 }
 console.log(solution('2(ab)k3(bc)')); // ababkbcbcbc
 console.log(solution('3(a2(b))ef')); // abbabbabbef
 
-// let answer = '';
-// let s = '2(ab)k3(bc)';
-
-
-
-// for (let x of s) {
-//     if (Number(x)) num += x;
-//     if (isNaN(Number(x)) && x !== '(' && x !== ')')  {
-//         str += x;
-//         if (num.length === 0) {
-//             answer+=str;
-//             str='';
-//         }
-//     }    
-//     if (x === ')') {
-//         for (let i = 0; i < num; i++) {
-//             answer += str;
-//         }
-//         num = '';
-//         str = '';
-//     }
-// }
-// console.log(num);
-// console.log(str);
-// console.log(answer);
-
-// for (let x of s) {
-//     if (Number(x)) {
-//         num += x;
-//     } else if (isNaN(Number(x)) && x !== '(') {
-//         str += x;
-//     } else if (x === ')') {
-//         for (let i = 0; i < num; i++) {
-//             answer += str;
-//         }
-//         num = '';
-//         str = '';
-//     }
-// }
-// console.log(answer);
-// s = s.split('');
-// console.log(s);
-// let numSt = [];
-// let strSt= [];
-
-// for (let i = 0; i < s.length; i++) {
-//     if (Number(s[i])) {
-//         numSt.push(s[i]);
-//         if (numSt.length > 1) {
-//             numSt = Number(numSt.join(''));
-//         }
-//     } else {
-//         strSt.push(s[i]);
-//     }
-//     if(s[i] === ')') {
-//         for (let i = 0; )
-//         answer.push
-//     }
-// }
-
-// let a = ['a', 'b'];
-// let b = ''; 
-// for(let i = 0; i < 3; i++) {
-//     b.push(a[])
-//     if (i === )
-// }
