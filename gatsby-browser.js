7206b2013472225874ad4ced130cd3b477a76763
freshhoe <yh91211@gmail.com>
2021-09-24 19:43:08 +0900

    fix: Modify GlobalStyle font-family

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,7 @@
  *
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
+import 'fontsource-noto-sans-kr';
+import 'fontsource-noto-sans-kr/700.css';
 import 'prismjs/themes/prism-tomorrow.css';
 // You can delete this file if you're not using it
